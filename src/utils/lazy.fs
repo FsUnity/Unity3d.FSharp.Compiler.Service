34f80b907ea6e7603a8b05073cc6f30ce586dee1
Jared Hester <jared.hester@gmail.com>
2015-08-02 00:11:03 -0400

    Added Lazy Module with Force
    Missing from .Net 3.5

---
+++ src/utils/lazy.fs
@@ -0,0 +1,14 @@
+﻿namespace Internal
+
+
+open System
+
+[<AutoOpen>]
+module Utilities =
+    type  Lazy<'T> with
+        member self.Force() = self.Value
+
+
+module Lazy = 
+    let force (x: Lazy<'T>) = x.Force()
+
